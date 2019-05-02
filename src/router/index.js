139cec6b477879a0bef16675902d0eae57884c61
Jonathan Dos Santos <jdsd45@gmail.com>
2019-05-02 18:31:13 +0200

    z

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/components/Home'
 import Prestations from '@/components/Prestations'
 import Contact from '@/components/Contact'
 import Keslene from '@/components/Keslene'
@@ -11,8 +11,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home',
+      component: Home
     },
     {
       path: '/prestations',
